673f80777c943305571175a35adfb5dc6092b5b9
unknown <r1m1nrao@PULT-TR01-04.hexa.training.local>
2019-11-08 11:49:44 +0530

    Initial Commit

---
+++ Helpers/StorageAccountHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityAPI.Helpers
+{
+    public class StorageAccountHelper
+    {
+        public string storageConnectionString;
+        private CloudStorageAccount storageAccount;
+
+        private CloudQueueClient queueClient;
+
+        public string StorageConnectionString
+        {
+            get { return storageConnectionString; }
+            set
+            {
+                this.storageConnectionString = value;
+                storageAccount = CloudStorageAccount.Parse(this.storageConnectionString);
+            }
+        }
+
+        public async Task SendMessageAsync(string messageText, string queueName)
+        {
+            queueClient = storageAccount.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference(queueName);
+            await queue.CreateIfNotExistsAsync();
+            CloudQueueMessage queueMessage = new CloudQueueMessage(messageText);
+            await queue.AddMessageAsync(queueMessage);
+        }
+    }
+}
